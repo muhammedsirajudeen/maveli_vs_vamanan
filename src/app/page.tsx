9de48063749c9224c972a1e00299770c5d19701d
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-09-01 21:46:54 +0530

    feat: preload winner image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import Head from "next/head";
 import Hero from "./components/hero";
 import { StickyCTA } from "./components/sticky-cta";
 
 export default function Page() {
   return (
-    <main>
-      <Hero />
-      <StickyCTA />
-    </main>
-  )
+    <>
+      <Head>
+        <link
+          rel="preload"
+          as="image"
+          href="https://onam.ciltriq.com/winner.png"
+        />
+      </Head>
+      <main>
+        <Hero />
+        <StickyCTA />
+      </main>
+    </>
+  );
 }
